9702eb4d7b7c9570aeee860a209b417dc6387667
Cody P Schafer <dev@codyps.com>
2014-09-22 16:34:43 -0400

    re-add tests

---
+++ tests/journal.rs
@@ -0,0 +1,17 @@
+#![feature(phase)]
+
+#[phase(plugin,link)] extern crate systemd;
+#[phase(plugin,link)] extern crate log;
+
+#[test]
+fn test() {
+    use log::{set_logger};
+    use systemd::journal;
+    journal::send(["CODE_FILE=HI", "CODE_LINE=1213", "CODE_FUNCTION=LIES"]);
+    journal::print(1, format!("Rust can talk to the journal: {}",
+                              4i).as_slice());
+    set_logger(box journal::JournalLogger);
+    log!(0, "HI");
+    sd_journal_log!(4, "HI {}", 2i);
+}
+
