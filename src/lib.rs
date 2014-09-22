c5a35f71bcba985820cc496f6fed934ddbccf55d
Cody P Schafer <dev@codyps.com>
2014-09-22 14:57:25 -0400

    Provide a sd_journal_log!(level, format, ...) macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 #![feature(phase)]
+#![feature(macro_rules)]
 
 extern crate libc;
 #[phase(plugin,link)]
 extern crate log;
+
 
 #[allow(dead_code)]
 mod posix {
@@ -25,7 +27,8 @@ mod systemd {
     pub mod journal {
         use libc::{c_char,c_int,c_void,size_t};
         use posix::const_iovec;
-        use log::{Logger,LogRecord,LogLevel};
+        use log::{Logger,LogRecord,LogLevel,LogLocation};
+        use std::fmt;
 
         #[link(name = "systemd")]
         extern {
@@ -59,21 +62,56 @@ mod systemd {
             ])
         }
 
+        pub fn log_(record: &LogRecord) {
+            let LogLevel(lvl) = record.level;
+            send([format!("PRIORITY={}", lvl).as_slice(),
+                  format!("MESSAGE={}", record.args).as_slice(),
+                  format!("CODE_LINE={}", record.line).as_slice(),
+                  format!("CODE_FILE={}", record.file).as_slice(),
+                  format!("CODE_FUNCTION={}", record.module_path).as_slice(),
+            ]);
+        }
+
+        pub fn log(level: u32, loc: &'static LogLocation, args: &fmt::Arguments)
+        {
+            log_(&LogRecord {
+                level: LogLevel(level),
+                args: args,
+                file: loc.file,
+                module_path: loc.module_path,
+                line: loc.line,
+            });
+        }
+
         pub struct JournalLogger;
         impl Logger for JournalLogger {
             fn log(&mut self, record: &LogRecord) {
-                let LogLevel(lvl) = record.level;
-                send([format!("PRIORITY={}", lvl).as_slice(),
-                      format!("MESSAGE={}", record.args).as_slice(),
-                      format!("CODE_LINE={}", record.line).as_slice(),
-                      format!("CODE_FILE={}", record.file).as_slice(),
-                      format!("MODULE_PATH={}", record.module_path).as_slice(),
-                    ]);
+                log_(record);
             }
         }
     }
 }
 
+/* This is nearly a clone of log!() except:
+*   - it accepts a func argument to invoke (instead of hard coding ::log::log())
+*   - it does not filter on log_enabled!()
+*/
+macro_rules! log_with(
+    ($func:expr, $lvl:expr, $($arg:tt)+) => ({
+        static LOC: ::log::LogLocation = ::log::LogLocation {
+            line: line!(),
+            file: file!(),
+            module_path: module_path!()
+        };
+        let lvl = $lvl;
+        let func = $func;
+        format_args!(|args| { func(lvl, &LOC, args) }, $($arg)+)
+    })
+)
+
+macro_rules! sd_journal_log(
+    ($lvl:expr, $($arg:tt)+) => (log_with!(::systemd::journal::log, $lvl, $($arg)+))
+)
 
 #[test]
 fn test() {
@@ -84,4 +122,5 @@ fn test() {
                               4i).as_slice());
     set_logger(box journal::JournalLogger);
     log!(0, "HI");
+    sd_journal_log!(4, "HI {}", 2i);
 }
