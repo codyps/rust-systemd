fc67a3e2a653ae7f5fac3012c0a338df5fcd8dc8
Cody P Schafer <dev@codyps.com>
2014-09-22 14:33:52 -0400

    logger trait support

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
+#![feature(phase)]
+
 extern crate libc;
+#[phase(plugin,link)]
+extern crate log;
 
 #[allow(dead_code)]
 mod posix {
@@ -21,17 +25,20 @@ mod systemd {
     pub mod journal {
         use libc::{c_char,c_int,c_void,size_t};
         use posix::const_iovec;
+        use log::{Logger,LogRecord,LogLevel};
+
         #[link(name = "systemd")]
         extern {
             /* printf() like variadic */
             fn sd_journal_print(priority : c_int, format : *const c_char, ...) -> c_int;
-            /* NULL terminated variadic */
-            fn sd_journal_send(format : *const c_char, ...) -> c_int;
             fn sd_journal_sendv(iv : *const const_iovec, n : c_int) -> c_int;
 
             fn sd_journal_print_with_location(prio: c_int, file_ish: *const c_char,
                                               line_ish: *const c_char, func: *const c_char,
                                               fmt: *const c_char, ...);
+            fn sd_journal_sendv_with_location(file_ish: *const c_char, line_ish: *const c_char,
+                                              func: *const c_char, iv: *const const_iovec,
+                                              n : c_int);
         }
 
         pub fn print(lvl : uint, s : &str) -> int {
@@ -40,18 +47,40 @@ mod systemd {
             }) as int
         }
 
-        pub fn send(args : &[&str]) -> c_int {
-            let iovecs: Vec<const_iovec> = args.iter().map(|d| {
+        fn array_to_iovecs(args: &[&str]) -> Vec<const_iovec> {
+            args.iter().map(|d| {
                 const_iovec { iov_base: d.as_ptr() as *const c_void, iov_len: d.len() as size_t }
-            }).collect();
+            }).collect()
+        }
+
+        pub fn send(args : &[&str]) -> c_int {
+            let iovecs = array_to_iovecs(args);
             unsafe { sd_journal_sendv(iovecs.as_ptr(), iovecs.len() as c_int) }
+        }
+
+        pub struct JournalLogger;
+        impl Logger for JournalLogger {
+            fn log(&mut self, record: &LogRecord) {
+                let LogLevel(lvl) = record.level;
+                send([format!("PRIORITY={}", lvl).as_slice(),
+                      format!("MESSAGE={}", record.args).as_slice(),
+                      format!("CODE_LINE={}", record.line).as_slice(),
+                      format!("CODE_FILE={}", record.file).as_slice(),
+                      format!("MODULE_PATH={}", record.module_path).as_slice(),
+                    ]);
+            }
         }
     }
 }
 
+
 #[test]
 fn test() {
     use systemd::journal;
-    journal::print(1, "Rust can talk to the journal");
+    use log::{set_logger};
     journal::send(["CODE_FILE=HI", "CODE_LINE=1213", "CODE_FUNCTION=LIES"]);
+    journal::print(1, format!("Rust can talk to the journal: {}",
+                              4i).as_slice());
+    set_logger(box journal::JournalLogger);
+    log!(0, "HI");
 }
