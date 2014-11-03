0f887b88d198ded950007ffb534cb80a82b05924
Steve McKay <steve@b.abbies.us>
2014-11-02 23:41:12 -0500

    Beginning work on reading the journal
    * Bindings for the rest of the journal APIs
    * Journal type with a few methods
    * sd_try macro to simplify FFI calls
    * Move FFI items to a separate module

---
+++ src/ffi.rs
@@ -0,0 +1,88 @@
+#![allow(non_camel_case_types)]
+
+use libc::{c_char,c_int,c_void,size_t};
+
+pub const SD_JOURNAL_LOCAL_ONLY:   c_int = 1;
+pub const SD_JOURNAL_RUNTIME_ONLY: c_int = 2;
+pub const SD_JOURNAL_SYSTEM:       c_int = 4;
+pub const SD_JOURNAL_CURRENT_USER: c_int = 8;
+
+#[repr(C)]
+pub struct iovec {
+    pub iov_base: *mut c_void,
+    pub iov_len: size_t
+}
+
+#[repr(C)]
+pub struct const_iovec {
+    pub iov_base: *const c_void,
+    pub iov_len: size_t
+}
+
+pub fn array_to_iovecs(args: &[&str]) -> Vec<const_iovec> {
+    args.iter().map(|d| {
+        const_iovec { iov_base: d.as_ptr() as *const c_void, iov_len: d.len() as size_t }
+    }).collect()
+}
+
+pub type sd_id128 = [u64, ..2];
+pub type sd_journal = *mut c_void;
+
+#[link(name = "systemd-journal")]
+extern {
+    pub fn sd_journal_sendv(iv : *const const_iovec, n : c_int) -> c_int;
+    /* There are a bunch of other send methods, but for rust it doesn't make sense to call them
+     * (we don't need to do c-style format strings) */
+    pub fn sd_journal_open(ret: *const sd_journal, flags: c_int) -> c_int;
+    pub fn sd_journal_close(j: sd_journal) -> ();
+
+    pub fn sd_journal_previous(j: sd_journal) -> c_int;
+    pub fn sd_journal_next(j: sd_journal) -> c_int;
+
+    pub fn sd_journal_previous_skip(j: sd_journal, skip: u64) -> c_int;
+    pub fn sd_journal_next_skip(j: sd_journal, skip: u64) -> c_int;
+
+    pub fn sd_journal_get_realtime_usec(j: sd_journal, ret: *const u64) -> c_int;
+    pub fn sd_journal_get_monotonic_usec(j: sd_journal, ret: *const u64, ret_boot_id: *const sd_id128) -> c_int;
+
+    pub fn sd_journal_set_data_threshold(j: sd_journal, sz: size_t) -> c_int;
+    pub fn sd_journal_get_data_threshold(j: sd_journal, sz: *mut size_t) -> c_int;
+
+    pub fn sd_journal_get_data(j: sd_journal, field: *const c_char, data: *const *mut u8, l: *mut size_t) -> c_int;
+    pub fn sd_journal_enumerate_data(j: sd_journal, data: *const *mut u8, l: *mut size_t) -> c_int;
+    pub fn sd_journal_restart_data(j: sd_journal) -> ();
+
+    pub fn sd_journal_add_match(j: sd_journal, data: *const c_void, size: size_t) -> c_int;
+    pub fn sd_journal_add_disjunction(j: sd_journal) -> c_int;
+    pub fn sd_journal_add_conjunction(j: sd_journal) -> c_int;
+    pub fn sd_journal_flush_matches(j: sd_journal) -> ();
+
+    pub fn sd_journal_seek_head(j: sd_journal) -> c_int;
+    pub fn sd_journal_seek_tail(j: sd_journal) -> c_int;
+    pub fn sd_journal_seek_monotonic_usec(j: sd_journal, boot_id: sd_id128, usec: u64) -> c_int;
+    pub fn sd_journal_seek_realtime_usec(j: sd_journal, usec: u64) -> c_int;
+    pub fn sd_journal_seek_cursor(j: sd_journal, cursor: *const c_char) -> c_int;
+
+    pub fn sd_journal_get_cursor(j: sd_journal, cursor: *const *mut c_char) -> c_int;
+    pub fn sd_journal_test_cursor(j: sd_journal, cursor: *const c_char) -> c_int;
+
+    pub fn sd_journal_get_cutoff_realtime_usec(j: sd_journal, from: *mut u64, to: *mut u64) -> c_int;
+    pub fn sd_journal_get_cutoff_monotonic_usec(j: sd_journal, boot_id: sd_id128, from: *mut u64, to: *mut u64) -> c_int;
+
+    pub fn sd_journal_get_usage(j: sd_journal, bytes: *mut u64) -> c_int;
+
+    pub fn sd_journal_query_unique(j: sd_journal, field: *const c_char) -> c_int;
+    pub fn sd_journal_enumerate_unique(j: sd_journal, data: *const *mut c_void, l: *mut size_t) -> c_int;
+    pub fn sd_journal_restart_unique(j: sd_journal) -> ();
+
+    pub fn sd_journal_get_fd(j: sd_journal) -> c_int;
+    pub fn sd_journal_get_events(j: sd_journal) -> c_int;
+    pub fn sd_journal_get_timeout(j: sd_journal, timeout_usec: *mut u64) -> c_int;
+    pub fn sd_journal_process(j: sd_journal) -> c_int;
+    pub fn sd_journal_wait(j: sd_journal, timeout_usec: u64) -> c_int;
+    pub fn sd_journal_reliable_fd(j: sd_journal) -> c_int;
+
+    pub fn sd_journal_get_catalog(j: sd_journal, text: *const *mut c_char) -> c_int;
+    pub fn sd_journal_get_catalog_for_message_id(id: sd_id128, ret: *const *mut c_char) -> c_int;
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,85 +1,26 @@
 #![feature(phase)]
 #![feature(macro_rules)]
 
+extern crate collections;
 extern crate libc;
 #[phase(plugin,link)] extern crate log;
 
-#[allow(dead_code)]
-mod posix {
-    use libc::{c_void,size_t};
-    #[repr(C)]
-    pub struct iovec {
-        pub iov_base: *mut c_void,
-        pub iov_len: size_t
-    }
-
-    #[repr(C)]
-    pub struct const_iovec {
-        pub iov_base: *const c_void,
-        pub iov_len: size_t
-    }
-}
-
-#[allow(dead_code)]
-pub mod journal {
-    use libc::{c_int,c_void,size_t};
-    use posix::const_iovec;
-    use log::{Logger,LogRecord,LogLevel,LogLocation};
-    use std::fmt;
-
-    #[link(name = "systemd-journal")]
-    extern {
-        fn sd_journal_sendv(iv : *const const_iovec, n : c_int) -> c_int;
-        /* There are a bunch of other methods, but for rust it doesn't make sense to call them (we
-         * don't need to do c-style format strings) */
-    }
-
-    fn array_to_iovecs(args: &[&str]) -> Vec<const_iovec> {
-        args.iter().map(|d| {
-            const_iovec { iov_base: d.as_ptr() as *const c_void, iov_len: d.len() as size_t }
-        }).collect()
-    }
-
-    pub fn send(args : &[&str]) -> c_int {
-        let iovecs = array_to_iovecs(args);
-        unsafe { sd_journal_sendv(iovecs.as_ptr(), iovecs.len() as c_int) }
-    }
-
-    pub fn print(lvl : uint, s : &str) -> c_int {
-        send([
-             format!("PRIORITY={}", lvl).as_slice(),
-             format!("MESSAGE={}", s).as_slice()
-        ])
-    }
-
-    pub fn log_(record: &LogRecord) {
-        let LogLevel(lvl) = record.level;
-        send([format!("PRIORITY={}", lvl).as_slice(),
-              format!("MESSAGE={}", record.args).as_slice(),
-              format!("CODE_LINE={}", record.line).as_slice(),
-              format!("CODE_FILE={}", record.file).as_slice(),
-              format!("CODE_FUNCTION={}", record.module_path).as_slice(),
-        ]);
-    }
-
-    pub fn log(level: u32, loc: &'static LogLocation, args: &fmt::Arguments)
-    {
-        log_(&LogRecord {
-            level: LogLevel(level),
-            args: args,
-            file: loc.file,
-            module_path: loc.module_path,
-            line: loc.line,
-        });
-    }
-
-    pub struct JournalLogger;
-    impl Logger for JournalLogger {
-        fn log(&mut self, record: &LogRecord) {
-            log_(record);
+#[macro_export]
+macro_rules! sd_try(
+    ($e:expr) => ({
+        let ret: i32;
+        unsafe {
+            ret = $e;
         }
-    }
-}
+        if ret < 0 {
+            return Err(::std::io::IoError::from_errno(ret.abs() as uint, false));
+        }
+        ret
+    })
+)
+
+pub mod ffi;
+pub mod journal;
 
 /* This is nearly a clone of log!() except:
 *   - it accepts a func argument to invoke (instead of hard coding ::log::log())
