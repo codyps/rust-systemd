7e7455d0450b678438f6fa58cac643dec89c3c1f
Kevin Rauwolf <sweetpea-git@tentacle.net>
2014-12-17 22:30:14 -0800

    Cleaned up warnings and compile errors for Rust nightly

--- tests/journal.rs
+++ tests/journal.rs
@@ -7,7 +7,7 @@
 fn test() {
     use log::{set_logger};
     use systemd::journal;
-    journal::send(["CODE_FILE=HI", "CODE_LINE=1213", "CODE_FUNCTION=LIES"]);
+    journal::send(&["CODE_FILE=HI", "CODE_LINE=1213", "CODE_FUNCTION=LIES"]);
     journal::print(1, format!("Rust can talk to the journal: {}",
                               4i).as_slice());
     set_logger(box journal::JournalLogger);
