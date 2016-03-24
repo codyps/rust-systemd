3125b01968478698e7eb571504e0ccf1e0d53038
kteza1 <k.teza1@gmail.com>
2016-03-24 20:39:06 +0530

    Adding seek_cursor and iterator implementation

--- src/journal.rs
+++ src/journal.rs
@@ -1,25 +1,23 @@
-use libc::{c_int,size_t};
-use log::{self,Log,LogRecord,LogLocation,SetLoggerError};
-use std::{fmt,ptr,result};
+use log::{self, Log, LogRecord, LogLocation, SetLoggerError};
+use std::{fmt, ptr, result};
 use std::collections::BTreeMap;
 use ffi;
 use super::Result;
+use libc::{self, free, c_int, c_char, size_t};
+use std::ffi::{CString, CStr};
 
 /// Send preformatted fields to systemd.
 ///
 /// This is a relatively low-level operation and probably not suitable unless
 /// you need precise control over which fields are sent to systemd.
-pub fn send(args : &[&str]) -> c_int {
+pub fn send(args: &[&str]) -> c_int {
     let iovecs = ffi::array_to_iovecs(args);
     unsafe { ffi::sd_journal_sendv(iovecs.as_ptr(), iovecs.len() as c_int) }
 }
 
 /// Send a simple message to systemd.
-pub fn print(lvl : u32, s : &str) -> c_int {
-    send(&[
-         &format!("PRIORITY={}", lvl),
-         &format!("MESSAGE={}", s)
-    ])
+pub fn print(lvl: u32, s: &str) -> c_int {
+    send(&[&format!("PRIORITY={}", lvl), &format!("MESSAGE={}", s)])
 }
 
 /// Send a `log::LogRecord` to systemd.
@@ -31,14 +29,12 @@ pub fn log_record(record: &LogRecord) {
     log(lvl, record.location(), record.args());
 }
 
-pub fn log(level: usize, loc: &LogLocation, args: &fmt::Arguments)
-{
+pub fn log(level: usize, loc: &LogLocation, args: &fmt::Arguments) {
     send(&[&format!("PRIORITY={}", level),
-        &format!("MESSAGE={}", args),
-        &format!("CODE_LINE={}", loc.line()),
-        &format!("CODE_FILE={}", loc.file()),
-        &format!("CODE_FUNCTION={}", loc.module_path()),
-    ]);
+           &format!("MESSAGE={}", args),
+           &format!("CODE_LINE={}", loc.line()),
+           &format!("CODE_FILE={}", loc.file()),
+           &format!("CODE_FUNCTION={}", loc.module_path())]);
 }
 
 pub struct JournalLog;
@@ -54,9 +50,7 @@ impl Log for JournalLog {
 
 impl JournalLog {
     pub fn init() -> result::Result<(), SetLoggerError> {
-        log::set_logger(|_max_log_level| {
-            Box::new(JournalLog)
-        })
+        log::set_logger(|_max_log_level| Box::new(JournalLog))
     }
 }
 
@@ -66,7 +60,8 @@ pub type JournalRecord = BTreeMap<String, String>;
 ///
 /// Supports read, next, previous, and seek operations.
 pub struct Journal {
-    j: ffi::sd_journal
+    j: ffi::sd_journal,
+    cursor: String,
 }
 
 /// Represents the set of journal files to read.
@@ -76,7 +71,7 @@ pub enum JournalFiles {
     /// The current user's journal.
     CurrentUser,
     /// Both the system-wide journal and the current user's journal.
-    All
+    All,
 }
 
 impl Journal {
@@ -103,10 +98,13 @@ impl Journal {
         flags |= match files {
             JournalFiles::System => ffi::SD_JOURNAL_SYSTEM,
             JournalFiles::CurrentUser => ffi::SD_JOURNAL_CURRENT_USER,
-            JournalFiles::All => 0
+            JournalFiles::All => 0,
         };
 
-        let journal = Journal { j: ptr::null_mut() };
+        let journal = Journal {
+            j: ptr::null_mut(),
+            cursor: "".to_string(),
+        };
         sd_try!(ffi::sd_journal_open(&journal.j, flags));
         sd_try!(ffi::sd_journal_seek_head(journal.j));
         Ok(journal)
@@ -114,7 +112,7 @@ impl Journal {
 
     /// Read the next record from the journal. Returns `io::EndOfFile` if there
     /// are no more records to read.
-    pub fn next_record(&self) -> Result<Option<JournalRecord>> {
+    pub fn next_record(&mut self) -> Result<Option<JournalRecord>> {
         if sd_try!(ffi::sd_journal_next(self.j)) == 0 {
             return Ok(None);
         }
@@ -135,7 +133,56 @@ impl Journal {
             }
         }
 
+        // update cursor
+        let mut c_cursor: *mut c_char = ptr::null_mut();
+        let cursor: String;
+        if sd_try!(ffi::sd_journal_get_cursor(self.j, &mut c_cursor)) == 0 {
+            unsafe {
+                // You should use Cstr for memory allocated by C
+                cursor = CStr::from_ptr(c_cursor as *const _)
+                             .to_string_lossy()
+                             .into_owned();
+            }
+            self.cursor = cursor;
+            unsafe {
+                free(c_cursor as *mut libc::c_void);
+            }
+        }
         Ok(Some(ret))
+    }
+
+    pub fn seek_cursor<S>(&self, position: S) -> Result<()>
+        where S: Into<String>
+    {
+        let position = position.into();
+        let c_position = CString::new(position.clone());
+        // seeks to latest time if cursor string is invalid
+        sd_try!(ffi::sd_journal_seek_cursor(self.j, c_position.unwrap().as_ptr() as *const _));
+        Ok(())
+    }
+}
+
+impl Iterator for Journal {
+    type Item = (JournalRecord, String);
+
+    fn next(&mut self) -> Option<(JournalRecord, String)> {
+        let next_record = self.next_record().unwrap();
+        let wait_time: u64 = 1 << 63;
+        match next_record {
+            Some(record) => Some((record, self.cursor.clone())),
+            None => {
+                let wait_time: u64 = 1 << 63;
+                let w_ret: i32;
+                unsafe {
+                    w_ret = ffi::sd_journal_wait(self.j, wait_time);
+                }
+                if w_ret <= 0 {
+                    None
+                } else {
+                    self.next()
+                }
+            }
+        }
     }
 }
 
@@ -148,4 +195,3 @@ impl Drop for Journal {
         }
     }
 }
-
--- tests/journal.rs
+++ tests/journal.rs
@@ -1,15 +1,31 @@
-#[macro_use] extern crate systemd;
-#[macro_use] extern crate log;
+#[macro_use]
+extern crate systemd;
+#[macro_use]
+extern crate log;
+use systemd::journal::{self, Journal, JournalFiles};
 
 #[test]
 fn test() {
-    use systemd::journal;
     journal::send(&["CODE_FILE=HI", "CODE_LINE=1213", "CODE_FUNCTION=LIES"]);
-    journal::print(1, &format!("Rust can talk to the journal: {}",
-                              4));
+    journal::print(1, &format!("Rust can talk to the journal: {}", 4));
 
     journal::JournalLog::init().ok().unwrap();
     log!(log::LogLevel::Info, "HI");
     sd_journal_log!(4, "HI {:?}", 2);
 }
 
+// #[test]
+fn iterator_test() {
+    let mut client = match Journal::open(JournalFiles::All, true, true) {
+        Ok(c) => c,
+        Err(e) => {
+            println!("Error opening");
+            panic!("Couldn't create client. Error = {:?}", e);
+        }
+    };
+    let mut count = 0;
+    for (journal, cursor) in client {
+        count += 1;
+        println!("{:?}", count);
+    }
+}
