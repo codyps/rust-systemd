5d90bc3b197cc099e36069bd6f407924d535f627
Cody P Schafer <dev@codyps.com>
2016-03-26 19:17:35 -0400

    id128: complete symbols

--- libsystemd-sys/src/id128.rs
+++ libsystemd-sys/src/id128.rs
@@ -1,5 +1,16 @@
+use super::{c_char, c_int};
 
 #[repr(C)]
 pub struct sd_id128_t {
     bytes: [u8;16]
+}
+
+extern "C" {
+    /* s: &[c_char;33] (note: probably non-mut */
+    pub fn sd_id128_to_string(id: sd_id128_t, s: *mut c_char) -> *mut c_char;
+
+    pub fn sd_id128_from_string(s: *const c_char, ret: *mut sd_id128_t) -> c_int;
+    pub fn sd_id128_randomize(ret: *mut sd_id128_t) -> c_int;
+    pub fn sd_id128_get_machine(ret: *mut sd_id128_t) -> c_int;
+    pub fn sd_id128_get_boot(ret: *mut sd_id128_t) -> c_int;
 }
