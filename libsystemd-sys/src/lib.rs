96c46aae5d38eebf35ddc70a19a65f3fd6af3d46
Cody P Schafer <dev@codyps.com>
2016-04-04 19:32:48 -0400

    sys/daemon: split into module

--- libsystemd-sys/src/lib.rs
+++ libsystemd-sys/src/lib.rs
@@ -96,28 +96,12 @@ extern {
 
     pub fn sd_journal_get_catalog(j: sd_journal, text: *const *mut c_char) -> c_int;
     pub fn sd_journal_get_catalog_for_message_id(id: sd_id128_t, ret: *const *mut c_char) -> c_int;
-
-    /* sd-daemon */
-    pub fn sd_listen_fds(unset_environment: c_int) -> c_int;
-    pub fn sd_is_fifo(fd: c_int, path: *const c_char) -> c_int;
-    pub fn sd_is_special(fd: c_int, path: *const c_char) -> c_int;
-    pub fn sd_is_socket(fd: c_int, family: c_int, sock_type: c_int, listening: c_int) -> c_int;
-    pub fn sd_is_socket_inet(fd: c_int, family: c_int, sock_type: c_int, listening: c_int, port: u16) -> c_int;
-    pub fn sd_is_socket_unix(fd: c_int, sock_type: c_int, listening: c_int, path: *const c_char, length: size_t) -> c_int;
-    pub fn sd_is_mq(fd: c_int, path: *const c_char) -> c_int;
-    pub fn sd_notify(unset_environment: c_int, state: *const c_char) -> c_int;
-    // skipping sd_*notifyf; ignoring format strings
-    pub fn sd_pid_notify(pid: pid_t, unset_environment: c_int, state: *const c_char) -> c_int;
-    pub fn sd_booted() -> c_int;
-    pub fn sd_watchdog_enabled(unset_environment: c_int, usec: *mut u64) -> c_int;
-
-
-
 }
 
 pub mod id128;
 pub mod event;
+pub mod daemon;
+pub use daemon::*;
 
 #[cfg(feature = "bus")]
 pub mod bus;
-
