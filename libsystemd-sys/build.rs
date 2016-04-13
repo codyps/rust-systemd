7556fed7d041d169d04f08a2734ad4ff576b1483
Cody P Schafer <dev@codyps.com>
2016-04-13 12:27:45 -0400

    sys: disable-python is apparently spelled without-python

--- libsystemd-sys/build.rs
+++ libsystemd-sys/build.rs
@@ -32,7 +32,7 @@ fn build_systemd() {
                 .arg("--enable-kdbus")
                 .arg("--disable-tests")
                 .arg("--disable-ldconfig")
-                .arg("--disable-python")
+                .arg("--without-python")
                 .arg("--disable-manpages"));
 
     let mut jobs : ffi::OsString = From::from("-j");
