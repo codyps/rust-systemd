08380b6e37f10763ba33a96f896491b5f9f97ef4
Luca Bruno <lucab@debian.org>
2016-07-17 22:56:00 +0200

    journal: implement seeking

--- src/journal.rs
+++ src/journal.rs
@@ -1,9 +1,13 @@
-use libc::{c_int, size_t};
+use libc::{c_char, c_int, size_t};
 use log::{self, Log, LogRecord, LogLocation, LogLevelFilter, SetLoggerError};
-use std::{fmt, ptr, result};
+use std::{fmt, io, ptr, result};
 use std::collections::BTreeMap;
+use std::ffi::CString;
+use std::io::ErrorKind::InvalidData;
 use ffi::array_to_iovecs;
+use ffi::id128::sd_id128_t;
 use ffi::journal as ffi;
+use id128::Id128;
 use super::Result;
 
 /// Send preformatted fields to systemd.
@@ -83,6 +87,23 @@ pub enum JournalFiles {
     All,
 }
 
+/// Seeking position in journal.
+pub enum JournalSeek {
+    Head,
+    Current,
+    Tail,
+    ClockMonotonic {
+        boot_id: Id128,
+        usec: u64,
+    },
+    ClockRealtime {
+        usec: u64,
+    },
+    Cursor {
+        cursor: String,
+    },
+}
+
 impl Journal {
     /// Open the systemd journal for reading.
     ///
@@ -140,6 +161,37 @@ impl Journal {
         }
 
         Ok(Some(ret))
+    }
+
+    /// Seek to a specific position in journal. On success, returns a cursor
+    /// to the current entry.
+    pub fn seek(&self, seek: JournalSeek) -> Result<String> {
+        match seek {
+            JournalSeek::Head => sd_try!(ffi::sd_journal_seek_head(self.j)),
+            JournalSeek::Current => 0,
+            JournalSeek::Tail => sd_try!(ffi::sd_journal_seek_tail(self.j)),
+            JournalSeek::ClockMonotonic { boot_id, usec } => {
+                sd_try!(ffi::sd_journal_seek_monotonic_usec(self.j,
+                                                            sd_id128_t {
+                                                                bytes: *boot_id.as_bytes(),
+                                                            },
+                                                            usec))
+            }
+            JournalSeek::ClockRealtime { usec } => {
+                sd_try!(ffi::sd_journal_seek_realtime_usec(self.j, usec))
+            }
+            JournalSeek::Cursor { cursor } => {
+                sd_try!(ffi::sd_journal_seek_cursor(self.j, cursor.as_ptr() as *const c_char))
+            }
+        };
+        let c: *mut c_char = ptr::null_mut();
+        if unsafe { ffi::sd_journal_get_cursor(self.j, &c) != 0 } {
+            // Cursor may need to be re-aligned on a real entry first.
+            sd_try!(ffi::sd_journal_next(self.j));
+            sd_try!(ffi::sd_journal_get_cursor(self.j, &c));
+        }
+        let cs = unsafe { CString::from_raw(c) };
+        cs.into_string().or(Err(io::Error::new(InvalidData, "invalid cursor")))
     }
 }
 
