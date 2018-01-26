a96e26c3a0ec9e2419cff9c7e37ac28d2891aae6
Cody P Schafer <dev@codyps.com>
2018-01-26 16:56:37 -0500

    libsystemd-sys/build.rs: print error from pkg-config if all probes fail
    Fixes #42

--- libsystemd-sys/build.rs
+++ libsystemd-sys/build.rs
@@ -2,10 +2,10 @@ extern crate pkg_config;
 use std::env;
 
 fn main() {
-    match pkg_config::find_library("libsystemd") {
+    let e = match pkg_config::find_library("libsystemd") {
         Ok(_) => return,
-        Err(..) => {}
-    }
+        Err(e) => e,
+    };
 
     match env::var("LIBSYSTEMD_LDFLAGS") {
         Ok(flags) => {
@@ -14,6 +14,9 @@ fn main() {
              */
             println!("cargo:rustc-flags={}", flags);
         }
-        Err(_) => panic!("systemd was not found via pkg-config nor via the env var LIBSYSTEMD_LDFLAGS"),
+        Err(_) => {
+            println!("{}", e);
+            panic!("systemd was not found via pkg-config nor via the env var LIBSYSTEMD_LDFLAGS")
+        },
     }
 }
