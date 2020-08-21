98cd4dcfcabdb5830fdee5126368a6de83609498
Cody P Schafer <dev@codyps.com>
2020-08-20 21:58:28 -0400

    libsystemd-sys: rework to match current standards
     - use build-env to handle `cargo:rerun-if-env-changed`
     - use variable names pattered after openssl-sys
     - remove direct rustc arg passing

--- libsystemd-sys/build.rs
+++ libsystemd-sys/build.rs
@@ -1,43 +1,51 @@
-extern crate pkg_config;
-use std::env;
+use std::path::Path;
 
 fn main() {
     #[cfg(not(feature = "elogind"))]
-    let library = pkg_config::find_library("libsystemd");
+    let name = "systemd";
     #[cfg(feature = "elogind")]
-    let library = pkg_config::find_library("libelogind");
+    let name = "elogind";
 
-    let error = match library {
+    let mut be = build_env::BuildEnv::from_env().unwrap();
+
+    let library_name = format!("lib{}", name);
+    let library = pkg_config::find_library(&library_name);
+
+    match library {
         Ok(_) => return,
-        Err(error) => error,
+        Err(error) => eprintln!("pkg_config could not find {}: {}", library_name, error),
     };
 
-    #[cfg(not(feature = "elogind"))]
-    let ld_preload_var = "LIBSYSTEMD_LDFLAGS";
-    #[cfg(feature = "elogind")]
-    let ld_preload_var = "LIBELOGIND_LDFLAGS";
+    let lib_var = format!("{}_LIBS", name.to_ascii_uppercase());
+    let lib_dir_var = format!("{}_LIB_DIR", name.to_ascii_uppercase());
 
-    match env::var(ld_preload_var) {
-        Ok(flags) => {
-            /* Ideally we'd avoid rustc-flags in favor of rustc-link-{search,lib}, but this should
-             * work fine
-             */
-            println!("cargo:rustc-flags={}", flags);
-            println!("cargo:rerun-if-env-changed=LIBSYSTEMD_LDFLAGS");
+    let libs = be.var(lib_var);
+    let lib_dir = match be.var(lib_dir_var.clone()) {
+        Some(lib_dir) => lib_dir,
+        None => {
+            panic!("Environment variable {} is required but is unset", lib_dir_var);
         }
-        Err(_) => {
-            eprintln!("{}", error);
+    };
 
-            #[cfg(not(feature = "elogind"))]
-            let lib_name = "systemd";
+    if !Path::new(&lib_dir).exists() {
+        panic!("{} refers to {:?}, which does not exist",
+            lib_dir_var, lib_dir);
+    }
 
-            #[cfg(feature = "elogind")]
-            let lib_name = "elogind";
+    println!(
+        "cargo:rustc-link-search=native={}",
+        lib_dir.to_string_lossy()
+    );
 
-            panic!(
-                "{} was not found via pkg-config nor via the env var {}",
-                lib_name, ld_preload_var
-            );
+    match libs {
+        Some(libs) => {
+            //let libs = libs.expect(&format!("non utf-8 value provided in {}", lib_var));
+            for lib in libs.into_string().unwrap().split(":") {
+                println!("cargo:rustc-link={}", lib);
+            }
+        }
+        None => {
+            println!("cargo:rustc-link={}", name);
         }
     }
 }
