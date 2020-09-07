005fda4a0f2ee0dd3c26f10226e47a31eb1b189e
Cody P Schafer <dev@codyps.com>
2020-09-06 21:39:29 -0400

    examples: add a low-level journal reader example

---
+++ examples/journal-reader.rs
@@ -0,0 +1,68 @@
+#[cfg(feature = "journal")]
+mod x {
+    //! Follow future journal log messages and print up to 100 of them.
+    use systemd::journal::{Journal, JournalFiles, JournalSeek};
+
+    const KEY_UNIT: &str = "_SYSTEMD_UNIT";
+    const KEY_MESSAGE: &str = "MESSAGE";
+
+    const MAX_MESSAGES: usize = 100;
+
+    pub fn main() -> Result<(), Box<dyn std::error::Error>> {
+        println!("Starting journal-logger");
+
+        // Open the journal
+        let runtime_only = false;
+        let local_only = false;
+        let mut reader = Journal::open(JournalFiles::All, runtime_only, local_only)
+            .expect("Could not open journal");
+
+        // Seek to end of current log to prevent old messages from being printed
+        reader
+            .seek(JournalSeek::Tail)
+            .expect("Could not seek to end of journal");
+
+        // Print up to MAX_MESSAGES incoming messages
+        let mut i = 0;
+        loop {
+            loop {
+                if reader.next()? == 0 {
+                    break;
+                }
+
+                let unit: Option<_> = reader.get_data(KEY_UNIT)?.and_then(|v| {
+                    v.value()
+                        .map(String::from_utf8_lossy)
+                        .map(|v| v.into_owned())
+                });
+                let message = reader.get_data(KEY_MESSAGE)?.and_then(|v| {
+                    v.value()
+                        .map(String::from_utf8_lossy)
+                        .map(|v| v.into_owned())
+                });
+
+                println!("[{:?}] {:?}", unit, message);
+
+                i += 1;
+                if i >= MAX_MESSAGES {
+                    eprintln!("done.");
+                    return Ok(());
+                }
+            }
+
+            reader.wait(None)?;
+        }
+    }
+}
+
+#[cfg(not(feature = "journal"))]
+mod x {
+    pub fn main() -> Result<(), Box<dyn std::error::Error>> {
+        println!("pass `--features journal`");
+        Ok(())
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    x::main()
+}
