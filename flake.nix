9209189a52c00231e6171f240f1543e6113d0654
Cody P Schafer <dev@codyps.com>
2025-07-17 01:16:52 -0400

    nix flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = ((import nixpkgs) {
+          inherit system;
+        });
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            rustc
+            cargo
+            rustfmt
+            sccache
+            clippy
+            rust-analyzer
+            systemd
+            pkg-config
+          ];
+
+          RUSTC_WRAPPER = "sccache";
+          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        };
+      }
+    );
+}
